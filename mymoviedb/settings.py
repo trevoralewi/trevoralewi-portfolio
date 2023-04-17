87d44cdf462e96eae3eb65f04a03bbe889071c99
Trevor Lewis <alextrevlewi@hotmail.com>
2023-04-17 06:58:43 -0400

    Prepare for Heroku deployment

--- mymoviedb/settings.py
+++ mymoviedb/settings.py
@@ -94,6 +94,11 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+if db_from_env:
+    DATABASES['default'].update(db_from_env)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
