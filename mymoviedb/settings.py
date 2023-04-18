25e16d62958fa7b462acddc1b11de37fcdca7fa1
Trevor Lewis <alextrevlewi@hotmail.com>
2023-04-18 13:37:54 -0400

    Updated settings.py for Heroku deployment

--- mymoviedb/settings.py
+++ mymoviedb/settings.py
@@ -139,6 +139,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
